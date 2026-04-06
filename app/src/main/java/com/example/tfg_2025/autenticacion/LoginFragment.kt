43f11371e32e549536eba06f3b5a9513018d0ec3
kacloyew <kar_yellow@hotmail.com>
2026-04-06 19:39:42 +0200

    commit

--- app/src/main/java/com/example/tfg_2025/autenticacion/LoginFragment.kt
+++ app/src/main/java/com/example/tfg_2025/autenticacion/LoginFragment.kt
@@ -1,7 +1,23 @@
-package com.example.tfg_2025.autenticacion
+package com.example.tfg_2025.ui.autenticacion
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.tfg_2025.R
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(vista: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(vista, savedInstanceState)
+
+
+        val botonIngresar = vista.findViewById<Button>(R.id.boton_ingresar)
+
+        botonIngresar.setOnClickListener {
+
+            findNavController().navigate(R.id.action_loginFragment_to_bibliotecaFragment)
+        }
+    }
 }
